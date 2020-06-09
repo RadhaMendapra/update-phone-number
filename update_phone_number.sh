200be293c9c45aa8e429a1b4eee30f2104c3cb54
Radha Mendapra <RadhaMendapra@users.noreply.github.com>
2020-06-09 13:57:07 -0400

    add the bash interpreter line

--- update_phone_number.sh
+++ update_phone_number.sh
@@ -1,4 +1,5 @@
 #script to update the phone number embedded in store's html pages
+#!/bin/bash
 
 #sed with regex to update the number
 sed -i -r 's/.*\(?([0-9]{3})\)?[- ]?[.]?([0-9A-Z]{3})[- ]?[.]?([0-9A-Z]{4})/202-221-1414/g' *.html
